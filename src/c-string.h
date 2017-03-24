486651831dc5bb2fc88e6657f5b6f22fe472f803
David Herrmann <dh.herrmann@gmail.com>
2017-03-24 16:43:17 +0100

    c-string: add hex encoder/decoder
    Add a simple hex encoder and decoder for C strings. Unlike the standard
    lib calls, it is independent of the locale.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/c-string.h
+++ src/c-string.h
@@ -46,6 +46,60 @@ _c_pure_ static inline char *c_string_prefix(const char *str, const char *prefix
         return !strncmp(str, prefix, l) ? (char *)str + l : NULL;
 }
 
+/**
+ * c_string_to_hex() - encode string as ascii-hex
+ * @str:        string to encode from
+ * @n:          length of @str in bytes
+ * @hex:        destination buffer
+ *
+ * This hex-encodes the source string into the destination buffer. The
+ * destination buffer must be at least twice as big as the source.
+ */
+static inline void c_string_to_hex(const char *str, size_t n, char *hex) {
+        static const char table[16] = "0123456789abcdef";
+
+        while (n--) {
+                *hex++ = table[(*str >> 4) & 0x0f];
+                *hex++ = table[(*str++) & 0x0f];
+        }
+}
+
+/**
+ * c_string_from_hex() - decode ascii-hex string
+ * @str:        string buffer to write into
+ * @n:          length of @str in bytes
+ * @hex:        hex encoded buffer to decode
+ *
+ * This hex-decodes @hex into the string buffer @str. Be aware that @hex must
+ * be twice the size as @str / @n.
+ *
+ * Return: True if successful, false if invalid.
+ */
+static inline bool c_string_from_hex(char *str, size_t n, const char *hex) {
+        static const uint8_t table[128] = {
+                 -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1, -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,
+                 -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1, -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,
+                 -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1, -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,
+                0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9,  -1,  -1, -1,  -1,  -1,  -1,
+                 -1, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf,  -1, -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,
+                 -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1, -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,
+                 -1, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf,  -1, -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,
+                 -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1, -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,
+        };
+        uint8_t v1, v2;
+
+        for ( ; n; --n, hex += 2) {
+                v1 = table[hex[0] & 0x7f];
+                v2 = table[hex[1] & 0x7f];
+                if (_c_unlikely_((hex[0] | hex[1] | v1 | v2) & 0x80))
+                        return false;
+
+                *str++ = (v1 << 4) | v2;
+        }
+
+        return true;
+}
+
 #ifdef __cplusplus
 }
 #endif
---
+++ src/test-string.c
@@ -0,0 +1,48 @@
+/*
+ * Tests for string manipulation helpers
+ */
+
+#include <stdlib.h>
+#include "c-macro.h"
+#include "c-string.h"
+
+static void test_verify_from_hex(const char *hex) {
+        _c_cleanup_(c_freep) char *raw = NULL, *copy = NULL;
+        bool valid_hex1, valid_hex2;
+        size_t n_hex;
+
+        /* a hex-string is valid _iff_ it consists of 0-9a-fA-F */
+
+        n_hex = strlen(hex);
+        raw = calloc(1, n_hex);
+        copy = calloc(1, n_hex);
+        assert(raw && copy);
+
+        valid_hex1 = !(n_hex % 2) && (n_hex == strspn(hex, "0123456789abcdefABCDEF"));
+        valid_hex2 = !(n_hex % 2) && c_string_from_hex(raw, n_hex / 2, hex);
+
+        assert(valid_hex1 == valid_hex2);
+
+        /* verify one round through c_string_from/to_hex() keeps the form */
+
+        if (valid_hex2) {
+                c_string_to_hex(raw, n_hex / 2, copy);
+                assert(!strncasecmp(hex, copy, n_hex));
+        }
+}
+
+/* test hex en/de-coders */
+static void test_hex(void) {
+        test_verify_from_hex("0");
+        test_verify_from_hex("00");
+        test_verify_from_hex("0a");
+        test_verify_from_hex("a" "0");
+        test_verify_from_hex("0123456789abcdefABCDEF");
+        test_verify_from_hex("a" "\x01");
+        test_verify_from_hex("\x01" "a");
+}
+
+int main(int argc, char **argv) {
+        test_hex();
+        return 0;
+}
