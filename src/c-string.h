d79cc4acb2eede3c9903b78790563dd9a6a51f8b
Tom Gundersen <teg@jklm.no>
2017-04-16 19:23:09 +0200

    c-string: add c_string_validate_{ascii,utf8}()
    These functions scan a stream of bytes to detect the first NULL
    or non-{ASCII,UTF-8} character.
    
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- src/c-string.h
+++ src/c-string.h
@@ -118,6 +118,149 @@ static inline bool c_string_from_hex(char *str, size_t n, const char *hex) {
         return true;
 }
 
+/**
+ * c_string_verify_ascii() - verify that a scring is ASCII encoded
+ * @strp:               pointer to string to verify
+ * @lenp:               pointer to length of string
+ *
+ * The first @lenp bytes of the string pointed to by @strp is
+ * verified to be ASCII encoded, and @strp and @lenp are updated to
+ * point to the first non-ASCII character or the first NULL of the
+ * sting, and the remaining number of bytes of the string, respectively.
+ */
+static inline void c_string_verify_ascii(char **strp, size_t *lenp) {
+        unsigned char *str = (unsigned char *) *strp;
+        size_t len = *lenp;
+
+        while (len > 0) {
+                if (_c_unlikely_(*str == 0x00 || *str > 0x7F))
+                        break;
+
+                ++str;
+                --len;
+        }
+
+        *strp = (char *) str;
+        *lenp = len;
+}
+
+/**
+ * c_string_verify_utf8() - verify that a scring is UTF-8 encoded
+ * @strp:               pointer to string to verify
+ * @lenp:               pointer to length of string
+ *
+ * Up to the first @lenp bytes of the string pointed to by @strp is
+ * verified to be UTF-8 encoded, and @strp and @lenp are updated to
+ * point to the first non-UTF-8 character or the first NULL of the
+ * string, and the remaining number of bytes of the string,
+ * respectively.
+ */
+static inline void c_string_verify_utf8(char **strp, size_t *lenp) {
+        unsigned char *str = (unsigned char *)*strp;
+        size_t len = *lenp;
+
+        /* See Unicode 9.0.0, Chapter 3, Section D92 */
+
+        while (len > 0) {
+                if (_c_unlikely_(*str == 0x00)) {
+                        break;
+                } else if (*str < 0x80) {
+                        ++str;
+                        --len;
+                } else if (_c_unlikely_(*str < 0xC2)) {
+                        break;
+                } else if (*str < 0xE0) {
+                        if (_c_unlikely_(len < 2))
+                                break;
+                        if (_c_unlikely_(*(str + 1) < 0x80 || *(str + 1) > 0xBF))
+                                break;
+
+                        str += 2;
+                        len -= 2;
+                } else if (*str < 0xE1) {
+                        if (_c_unlikely_(len < 3))
+                                break;
+                        if (_c_unlikely_(*(str + 1) < 0xA0 || *(str + 1) > 0xBF))
+                                break;
+                        if (_c_unlikely_(*(str + 2) < 0x80 || *(str + 2) > 0xBF))
+                                break;
+
+                        str += 3;
+                        len -= 3;
+                } else if (*str < 0xED) {
+                        if (_c_unlikely_(len < 3))
+                                break;
+                        if (_c_unlikely_(*(str + 1) < 0x80 || *(str + 1) > 0xBF))
+                                break;
+                        if (_c_unlikely_(*(str + 2) < 0x80 || *(str + 2) > 0xBF))
+                                break;
+
+                        str += 3;
+                        len -= 3;
+                } else if (*str < 0xEE) {
+                        if (_c_unlikely_(len < 3))
+                                break;
+                        if (_c_unlikely_(*(str + 1) < 0x80 || *(str + 1) > 0x9F))
+                                break;
+                        if (_c_unlikely_(*(str + 2) < 0x80 || *(str + 2) > 0xBF))
+                                break;
+
+                        str += 3;
+                        len -= 3;
+                } else if (*str < 0xF0) {
+                        if (_c_unlikely_(len < 3))
+                                break;
+                        if (_c_unlikely_(*(str + 1) < 0x80 || *(str + 1) > 0xBF))
+                                break;
+                        if (_c_unlikely_(*(str + 2) < 0x80 || *(str + 2) > 0xBF))
+                                break;
+
+                        str += 3;
+                        len -= 3;
+                } else if (*str < 0xF1) {
+                        if (_c_unlikely_(len < 4))
+                                break;
+                        if (_c_unlikely_(*(str + 1) < 0x90 || *(str + 1) > 0xBF))
+                                break;
+                        if (_c_unlikely_(*(str + 2) < 0x80 || *(str + 2) > 0xBF))
+                                break;
+                        if (_c_unlikely_(*(str + 3) < 0x80 || *(str + 3) > 0xBF))
+                                break;
+
+                        str += 4;
+                        len -= 4;
+                } else if (*str < 0xF4) {
+                        if (_c_unlikely_(len < 4))
+                                break;
+                        if (_c_unlikely_(*(str + 1) < 0x80 || *(str + 1) > 0xBF))
+                                break;
+                        if (_c_unlikely_(*(str + 2) < 0x80 || *(str + 2) > 0xBF))
+                                break;
+                        if (_c_unlikely_(*(str + 3) < 0x80 || *(str + 3) > 0xBF))
+                                break;
+
+                        str += 4;
+                        len -= 4;
+                } else if (*str < 0xF5) {
+                        if (_c_unlikely_(len < 4))
+                                break;
+                        if (_c_unlikely_(*(str + 1) < 0x80 || *(str + 1) > 0x8F))
+                                break;
+                        if (_c_unlikely_(*(str + 2) < 0x80 || *(str + 2) > 0xBF))
+                                break;
+                        if (_c_unlikely_(*(str + 3) < 0x80 || *(str + 3) > 0xBF))
+                                break;
+
+                        str += 4;
+                        len -= 4;
+                } else
+                        break;
+        }
+
+        *strp = (char *)str;
+        *lenp = len;
+}
+
 #ifdef __cplusplus
 }
 #endif
--- src/test-string.c
+++ src/test-string.c
@@ -80,9 +80,44 @@ static void test_hex(void) {
         test_verify_from_hex("\x01" "a");
 }
 
+static void test_ascii(void) {
+        char str[0x100];
+        char *p = str;
+        size_t len = sizeof(str);
+
+        for (size_t i = 0; i < sizeof(str); ++i)
+                str[i] = i;
+
+        c_string_verify_ascii(&p, &len);
+        assert(*p == 0x00);
+        assert(p == str);
+        assert(len = sizeof(str));
+
+        ++p;
+        --len;
+
+        c_string_verify_ascii(&p, &len);
+        assert((unsigned char)*p == 0x80);
+        assert(p = str + 0xFF);
+        assert(len  = sizeof(str) - 0xFF);
+}
+
+static void test_utf8(void) {
+        char *str = "Η Ελλάδα ή Ελλάς, επίσημα γνωστή ως Ελληνική Δημοκρατία, είναι χώρα της νοτιοανατολικής Ευρώπης στο νοτιότερο άκρο της Βαλκανικής χερσονήσου. Συνορεύει στα βορειοδυτικά με την Αλβανία, στα βόρεια με την πρώην Γιουγκοσλαβική Δημοκρατία της Μακεδονίας και τη Βουλγαρία και στα βορειοανατολικά με την Τουρκία. Česko, úředním názvem Česká republika, je stát ve střední Evropě. Jako formálně svrchovaný národní stát vznikla tehdejší Česká socialistická republika 1. ledna 1969 v rámci federalizace Československa. Od 6. března 1990 nese tento stát název Česká republika. 中華民國十年，中國共產黨立於上海。初附於中國國民黨，黨人得以兼國民黨，共理中華民國廣州軍政府，同謀北伐。其後國民黨人以共產黨人以公務營黨務，既下南京，蔣中正令捕殺共產黨人。遂奔江西。十六年起義於南昌，中國工農紅軍是立";
+        char *p = str;
+        size_t len = strlen(str) + 1;
+
+        c_string_verify_utf8(&p, &len);
+        assert(len == 1);
+        assert(p == str + strlen(str));
+        assert(*p == 0x00);
+}
+
 int main(int argc, char **argv) {
         test_compare();
         test_equal();
         test_hex();
+        test_ascii();
+        test_utf8();
         return 0;
 }
