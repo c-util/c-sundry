f78ce59f64631592bc49b797f10394628506cfd2
David Herrmann <dh.herrmann@gmail.com>
2017-07-04 10:40:10 +0200

    test/string: avoid variable shadowing
    Indent the first utf8 test so we no longer produce shadowing warnings
    for all the following tests.
    
    While at it, properly format the utf8 string and use the right type.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/test-string.c
+++ src/test-string.c
@@ -103,15 +103,34 @@ static void test_ascii(void) {
 }
 
 static void test_utf8(void) {
-        char *str = "Η Ελλάδα ή Ελλάς, επίσημα γνωστή ως Ελληνική Δημοκρατία, είναι χώρα της νοτιοανατολικής Ευρώπης στο νοτιότερο άκρο της Βαλκανικής χερσονήσου. Συνορεύει στα βορειοδυτικά με την Αλβανία, στα βόρεια με την πρώην Γιουγκοσλαβική Δημοκρατία της Μακεδονίας και τη Βουλγαρία και στα βορειοανατολικά με την Τουρκία. Česko, úředním názvem Česká republika, je stát ve střední Evropě. Jako formálně svrchovaný národní stát vznikla tehdejší Česká socialistická republika 1. ledna 1969 v rámci federalizace Československa. Od 6. března 1990 nese tento stát název Česká republika. 中華民國十年，中國共產黨立於上海。初附於中國國民黨，黨人得以兼國民黨，共理中華民國廣州軍政府，同謀北伐。其後國民黨人以共產黨人以公務營黨務，既下南京，蔣中正令捕殺共產黨人。遂奔江西。十六年起義於南昌，中國工農紅軍是立";
-        char *p = str;
-        size_t len = strlen(str) + 1;
-
         /* verify a mix of greek, czech and chinese */
-        c_string_verify_utf8(&p, &len);
-        assert(len == 1);
-        assert(p == str + strlen(str));
-        assert(*p == 0x00);
+        {
+                char str[] = "Η Ελλάδα ή Ελλάς, επίσημα γνωστή ως Ελληνική "
+                             "Δημοκρατία, είναι χώρα της νοτιοανατολικής "
+                             "Ευρώπης στο νοτιότερο άκρο της Βαλκανικής "
+                             "χερσονήσου. Συνορεύει στα βορειοδυτικά με την "
+                             "Αλβανία, στα βόρεια με την πρώην Γιουγκοσλαβική "
+                             "Δημοκρατία της Μακεδονίας και τη Βουλγαρία και "
+                             "στα βορειοανατολικά με την Τουρκία. Česko, "
+                             "úředním názvem Česká republika, je stát ve "
+                             "střední Evropě. Jako formálně svrchovaný národní "
+                             "stát vznikla tehdejší Česká socialistická "
+                             "republika 1. ledna 1969 v rámci federalizace "
+                             "Československa. Od 6. března 1990 nese tento "
+                             "stát název Česká republika. 中華民國十年，"
+                             "中國共產黨立於上海。初附於中國國民黨，"
+                             "黨人得以兼國民黨，共理中華民國廣州軍政府，"
+                             "同謀北伐。其後國民黨人以共產黨人以公務營黨務，"
+                             "既下南京，蔣中正令捕殺共產黨人。遂奔江西。"
+                             "十六年起義於南昌，中國工農紅軍是立";
+                char *p = str;
+                size_t len = sizeof(str);
+
+                c_string_verify_utf8(&p, &len);
+                assert(len == 1);
+                assert(p == str + strlen(str));
+                assert(*p == 0x00);
+        }
 
         /* verify every 1-byte character */
         for (uint32_t i = 0; i <= 0xFF; i++) {
